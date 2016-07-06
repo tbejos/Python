0dff135ebbb20537b2682c8a8897ddedf7ff4b76
Txanton <txb2@live.com>
2016-07-06 19:34:17 -0400

    Update renamingtool.py

--- renamingtool.py
+++ renamingtool.py
@@ -1,7 +1,10 @@
+# Author: Txanton Bejos
 import os
 
-
+# for all files in source folder
 for file in os.listdir(os.getcwd()):
+    #if "_" present in file name
     if file.find("_") >= 0:
         newName = file.replace("_", " ")
+        #Rename file with same name replacing underscores with spaces
         os.rename(file, newName)
