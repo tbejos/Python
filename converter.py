cd7902ec2fbaa69e621a6f2992e979500fa87bfb
Txanton <txb2@live.com>
2016-07-06 19:31:08 -0400

    Update converter.py

--- converter.py
+++ converter.py
@@ -10,7 +10,6 @@ for file in os.listdir(os.getcwd()):
     if file[-3:] == "mkv" and file[-7:] != "new.mkv":
         # print file name minus extension
         print(file[:file.rfind(".")])
-        # format
         # "command name", "-input", source file, "-codec:video", "copy from source",
         #"-codec:audio", "AAC", "-bitrate:audio", "384 kbps", output file (same name as input + " - new")
         command = ["ffmpeg", "-i", file, "-c:v", "copy", "-c:a", "aac", "-b:a", "384k", file[:-4]+" - new.mkv"]
