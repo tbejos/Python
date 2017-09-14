546dd385ba7666209a6054fc7f0a106774c45292
Txanton Bejos <txb2@live.com>
2017-09-14 18:20:06 -0400

    Cleaned up some Scripts

--- Scripts/converter.py
+++ Scripts/converter.py
@@ -3,14 +3,11 @@
 import os
 import subprocess
 
-# for all files in current directory
 for file in os.listdir(os.getcwd()):
-    # if file extension == .mkv but is not "new.mkv"
     if file[-3:] == "mkv" and file[-7:] != "new.mkv":
         # print file name minus extension
         print(file[:file.rfind(".")])
         # "command name", "-input", source file, "-codec:video", "copy from source",
         #"-codec:audio", "AAC", "-bitrate:audio", "384 kbps", output file (same name as input + " - new")
         command = ["ffmpeg", "-i", file, "-c:v", "copy", "-c:a", "aac", "-b:a", "384k", file[:-4]+" - new.mkv"]
-        #inputs command into the terminal/command prompt
         subprocess.call(command)
--- Scripts/renamingtool.py
+++ Scripts/renamingtool.py
@@ -1,10 +1,11 @@
 # Author: Txanton Bejos
 import os
 
-# for all files in source folder
 for file in os.listdir(os.getcwd()):
-    #if "_" present in file name
     if file.find("_") >= 0:
         newName = file.replace("_", " ")
-        #Rename file with same name replacing underscores with spaces
         os.rename(file, newName)
+    elif file.find(".") > 0:
+        newName = file.replace(".", " ", file.count(".") - 1)
+        os.rename(file, newName)
+
