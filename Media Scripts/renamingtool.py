b8e6c8587fffe319d85b2c1f6d74cac0967bedcd
Txanton <txb2@live.com>
2018-06-09 02:23:18 -0400

    Update renamingtool.py

--- Media Scripts/renamingtool.py
+++ Media Scripts/renamingtool.py
@@ -1,15 +1,29 @@
 # Author: Txanton Bejos
 import os
+import re
 
-#Webrip
-badwords = ["x264", "x265", "BluRay", "YIFY"]
+# Webrip
+badwords = ["x264", "x265", "BluRay", "YIFY", "YTS AG", "YTS AM", "BrRip"]
+pattern = re.compile(r'.[0-9]{4}.')  # String of numbers length 4
 
 for file in os.listdir(os.getcwd()):
     newName = file
     newName = newName.replace("_", " ")
     newName = newName.replace("[", "").replace("]", "")
+
+    beginning = re.search(pattern, newName)
+    if beginning:
+        year = newName[beginning.start() + 1: beginning.end() - 1]
+        newName = newName[:beginning.start()] + ".(" + year + ")." + \
+                  newName[beginning.end():]
+
+    # We do this afterwards because if the movie title has a year in it then
+    # this will make sure we don't modify it
     newName = newName.replace(".", " ", file.count(".") - 1)
     for word in badwords:
         newName = newName.replace(word, "")
     newName = ' '.join(newName.split())
+    # Remove any trailing spaces between resolution and file extension
+    fileExtension = newName.rfind(".")
+    newName = newName[:fileExtension].rstrip() + newName[fileExtension:]
     os.rename(file, newName)
