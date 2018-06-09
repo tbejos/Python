935ca08be1c2e493c8e513f64aee9eb7fcb9520b
Txanton <txb2@live.com>
2018-06-09 04:43:54 -0400

    Update renamingtool.py

--- Media Scripts/renamingtool.py
+++ Media Scripts/renamingtool.py
@@ -1,29 +1,42 @@
-# Author: Txanton Bejos
+__author__ =  "Txanton Bejos"
+
 import os
 import re
 
 # Webrip
 badwords = ["x264", "x265", "BluRay", "YIFY", "YTS AG", "YTS AM", "BrRip"]
 pattern = re.compile(r'.[0-9]{4}.')  # String of numbers length 4
+res = re.compile(r'[0-9]{3,4}p')
 
-for file in os.listdir(os.getcwd()):
-    newName = file
-    newName = newName.replace("_", " ")
-    newName = newName.replace("[", "").replace("]", "")
+def start(path):
+    print("###")
+    for file in os.listdir(path):
+        if file[0] == "." or file[0:2] == "__":
+            continue
+        print(file)
+        newName = file
+        newName = newName.replace("_", " ")
+        newName = newName.replace("[", "").replace("]", "")
 
-    beginning = re.search(pattern, newName)
-    if beginning:
-        year = newName[beginning.start() + 1: beginning.end() - 1]
-        newName = newName[:beginning.start()] + ".(" + year + ")." + \
-                  newName[beginning.end():]
+        beginning = re.search(pattern, newName)
+        if beginning:
+            year = newName[beginning.start() + 1: beginning.end() - 1]
+            newName = newName[:beginning.start()] + " (" + year + ") " + \
+                      newName[beginning.end():]
 
-    # We do this afterwards because if the movie title has a year in it then
-    # this will make sure we don't modify it
-    newName = newName.replace(".", " ", file.count(".") - 1)
-    for word in badwords:
-        newName = newName.replace(word, "")
-    newName = ' '.join(newName.split())
-    # Remove any trailing spaces between resolution and file extension
-    fileExtension = newName.rfind(".")
-    newName = newName[:fileExtension].rstrip() + newName[fileExtension:]
-    os.rename(file, newName)
+        # We do this afterwards because if the movie title has a year in it then
+        # this will make sure we don't modify it
+        newName = newName.replace(".", " ", file.count(".") - 1)
+        for word in badwords:
+            newName = newName.replace(word, "")
+
+        # Removes any left over .'s from unrecognized words
+        newName = newName.replace(".", " ", file.count(".") - 1)
+        newName = ' '.join(newName.split())
+        # Remove any trailing spaces between resolution and file extension
+        resol = re.search(res, newName)
+        if resolution and newName.rfind(".") >= 0:
+            newName = newName[:resol.end() + 1] + '.' + os.path.splitext(file)[1]
+        os.rename(path + os.sep + file, path + os.sep + newName)
+        print(newName)
+    print("###")
